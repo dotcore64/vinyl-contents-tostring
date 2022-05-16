8a8dd9c979083a5c65789d9cbfd9d113d74bb7a4
Julian Grinblat <julian@dotcore.co.il>
2022-05-16 12:19:38 +0900

    Use p-from-callback

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-// import { isVinyl } from 'vinyl';
-import Vinyl from 'vinyl'; // TODO: Use named imports when available
 import { pipeline } from 'node:stream/promises';
+import Vinyl from 'vinyl';
 import bl from 'bl';
+import fromCallback from 'p-from-callback';
 
-const { isVinyl } = Vinyl;
-const fromCallback = (fn) => new Promise(
-  (resolve, reject) => { fn((err, res) => (err ? reject(err) : resolve(res))); },
-);
-
+const { isVinyl } = Vinyl; // TODO: Use named imports when available
 export default (file, enc) => (!isVinyl(file) // eslint-disable-line no-nested-ternary
   ? Promise.reject(new TypeError('First argument must be a Vinyl file'))
   : file.isBuffer() // eslint-disable-line no-nested-ternary
     ? Promise.resolve(file.contents.toString(enc))
     : file.isStream()
-      : Promise.resolve(''));
       ? fromCallback((cb) => pipeline(file.contents, bl(cb))).then((buf) => buf.toString(enc))
+      : Promise.resolve(''));

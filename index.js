9cb20b70bfa005e95bb0c2be1112b03c241253a7
Julian Grinblat <julian@dotcore.co.il>
2022-05-16 13:28:26 +0900

    Replace stream-to-string with bl (#121)
    * Replace stream-to-string with bl
    
    * Use p-from-callback
    
    * Attempt fix
    
    * Support node 14

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// import { isVinyl } from 'vinyl';
-import Vinyl from 'vinyl'; // TODO: Use named imports when available
-import streamToString from 'stream-to-string';
+import { pipeline } from 'node:stream';
+import Vinyl from 'vinyl';
+import bl from 'bl';
+import fromCallback from 'p-from-callback';
 
-const { isVinyl } = Vinyl;
-
+const noop = () => {}; // TODO: remove and replace with node:stream/promises (node>=16)
+const { isVinyl } = Vinyl; // TODO: Use named imports when available
 export default (file, enc) => (!isVinyl(file) // eslint-disable-line no-nested-ternary
   ? Promise.reject(new TypeError('First argument must be a Vinyl file'))
   : file.isBuffer() // eslint-disable-line no-nested-ternary
     ? Promise.resolve(file.contents.toString(enc))
     : file.isStream()
-      ? streamToString(file.contents, enc)
+      ? fromCallback((cb) => pipeline(file.contents, bl(cb), noop)).then((buf) => buf.toString(enc))
       : Promise.resolve());

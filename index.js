3facc8003b8fcf5a48c447d17e954f679b98f1a6
Julian Grinblat <julian@dotcore.co.il>
2024-05-01 15:08:14 +0900

    chore: drop node 16 and add node 22

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import { pipeline } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
 import Vinyl from 'vinyl';
 import bl from 'bl';
 import fromCallback from 'p-from-callback';
 
-const noop = () => {}; // TODO: remove and replace with node:stream/promises (node>=16)
 const { isVinyl } = Vinyl; // TODO: Use named imports when available
 export default (file, enc) => (isVinyl(file)
   ? file.isBuffer()
     ? Promise.resolve(file.contents.toString(enc))
     : file.isStream()
-      ? fromCallback((cb) => pipeline(file.contents, bl(cb), noop)).then((buf) => buf.toString(enc))
+      ? fromCallback((cb) => pipeline(file.contents, bl(cb))).then((buf) => buf.toString(enc))
       : Promise.resolve()
   : Promise.reject(new TypeError('First argument must be a Vinyl file')));

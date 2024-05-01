386c47f5a6d4f700effb3f951931a35e20548a90
Julian Grinblat <julian@dotcore.co.il>
2024-05-01 15:21:38 +0900

    simplify

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { pipeline } from 'node:stream/promises';
 import Vinyl from 'vinyl';
 import bl from 'bl';
 import fromCallback from 'p-from-callback';
@@ -8,6 +7,6 @@ export default (file, enc) => (isVinyl(file)
   ? file.isBuffer()
     ? Promise.resolve(file.contents.toString(enc))
     : file.isStream()
-      ? fromCallback((cb) => pipeline(file.contents, bl(cb))).then((buf) => buf.toString(enc))
+      ? fromCallback((cb) => file.contents.pipe(bl(cb))).then((buf) => buf.toString(enc))
       : Promise.resolve()
   : Promise.reject(new TypeError('First argument must be a Vinyl file')));

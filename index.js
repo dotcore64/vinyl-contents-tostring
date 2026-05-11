8e1bcd7165643903d89c8a7bf902db348a5d0f72
Julian Grinblat <julian@dotcore.co.il>
2026-05-11 10:31:28 +0900

    fix: breaking changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Vinyl from 'vinyl';
-import bl from 'bl';
+import { BufferListStream } from 'bl';
 import fromCallback from 'p-from-callback';
 
 const { isVinyl } = Vinyl; // TODO: Use named imports when available
@@ -7,6 +7,8 @@ export default (file, enc) => (isVinyl(file)
   ? file.isBuffer()
     ? Promise.resolve(file.contents.toString(enc))
     : file.isStream()
-      ? fromCallback((cb) => file.contents.pipe(bl(cb))).then((buf) => buf.toString(enc))
+      ? fromCallback(
+        (cb) => file.contents.pipe(new BufferListStream(cb)),
+      ).then((buf) => buf.toString(enc))
       : Promise.resolve()
   : Promise.reject(new TypeError('First argument must be a Vinyl file')));
